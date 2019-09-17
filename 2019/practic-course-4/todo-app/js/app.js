d2050fd69c6b712eccbc5a9676877bba309c79e4
lulldev <lulldev@yandex.ru>
2019-09-17 15:43:18 +0300

    simplify lw2

--- 2019/practic-course-4/todo-app/js/app.js
+++ 2019/practic-course-4/todo-app/js/app.js
@@ -1,39 +1,66 @@
-$(document).ready(function () {
-  var btn = $('#btn');
-  // btn.prop("disabled", true);
-  var textarea = $('#textarea');
 
-  btn.click(function () {
-    addTask();
-    textarea.val('');
-  });
+window.onload = function () {
+  var btn = document.getElementById('button');
+  var txtArea = document.getElementById('textarea');
 
-  function addTask() {
-    var taskContent = textarea.val();
-    var containerToDo = $('#container-to-do');
-    var newTask = $('<div>');
-    newTask.addClass('new');
-    newTask.addClass('checkbox');
+  // add task handler
+  btn.onclick = function () {
+    // add element to UI
+    var taskContent = txtArea.value;
+    var containerToDo = document.getElementById('container-to-do');
+    var newTask = document.createElement('div');
+    newTask.classList.add('new', 'checkbox');
     containerToDo.append(newTask);
-    var label = $('<label>');
-    var check = $('<input>');
-    check.attr('type', 'checkbox');
+    var label = document.createElement('label');
+    var check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
     label.append(check);
     newTask.append(label);
-    var text = $(document.createTextNode(taskContent));
+    var text = document.createTextNode(taskContent);
     label.append(text);
-    var icon = $('<i>');
-    icon.addClass('glyphicon glyphicon-trash icon');
+    var icon = document.createElement('i');
+    icon.classList.add('glyphicon', 'glyphicon-trash', 'icon', 'remove-card');
     newTask.append(icon);
+    txtArea.value = "";
 
-    textarea.val('')
+    // register events here:
 
-    check.click(function () {
-      labeledTask();
-    });
+    // complete task
+    // TODO: labeled - not valid name for CSS and context
+    check.onclick = function () {
+      label.classList.toggle('labeled');
+    };
 
-    function labeledTask() {
-      label.toggleClass('labeled');
-    }
-  }
-});
+    // TODO: delete task event
+    icon.onclick = function () {
+      console.log('delete');
+    };
+  };
+
+  // TODO: complete all handler here
+  /*
+    var completeAll = document.getElementById('complete-all');
+    completeAll.onclick = function () {
+      console.log('complete all');
+    };
+  */
+
+
+  // TODO: filter tasks
+  /*
+    var filterShowAll = document.getElementById('filter-show-all');
+    filterShowAll.onclick = function () {
+      console.log('show all');
+    };
+
+    var filterShowCompleted = document.getElementById('filter-show-completed');
+    filterShowCompleted.onclick = function () {
+      console.log('show completed');
+    };
+
+    var filterShowCompleted = document.getElementById('filter-show-removed');
+    filterShowRemoved.onclick = function () {
+      console.log('show removed');
+    };
+  */
+}
