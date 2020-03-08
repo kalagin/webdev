05b3b7d138632aa18b2a6fe92005706f1650e36b
Ivan Kalagin <ikalagin@onlinetours.ru>
2020-03-08 12:24:53 +0300

    Fix code error

--- workshops/nodejs-backend/server.js
+++ workshops/nodejs-backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 // для того, чтобы можно было обрабатывать req.body
 app.use(bodyParser.text());
